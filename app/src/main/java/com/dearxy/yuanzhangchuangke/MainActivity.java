a28dca1ad2f8ff4d5c20cf1c86c1abcd08340a53
ITZhouXu <374952705@qq.com>
2017-04-05 10:20:13 +0800

    Initial commit

---
+++ app/src/main/java/com/dearxy/yuanzhangchuangke/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dearxy.yuanzhangchuangke;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * 院长创客APP
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
